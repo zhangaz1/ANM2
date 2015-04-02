d1a60396d64ea3b354324a0f2ef7447405cae83c
zhangaz1 <zhangaz1@hotmail.com>
2015-04-02 16:10:51 +0800

    reflector： add tasks by task/index.js

--- gulp/index.js
+++ gulp/index.js
@@ -5,9 +5,5 @@ var gulp = require('gulp');
 
 var config = require('./config.js');
 
-gulp.task(
-	config.tasks.default,
-	function(done) {
-		console.log('default task run!');
-		done(null);
-	});
+require('./tasks/index.js')
+	.createTasks(gulp, config);
--- gulp/tasks/index.js
+++ gulp/tasks/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+exports.createTasks = function(gulp, config) {
+	gulp.task(
+		config.tasks.default,
+		function(done) {
+			console.log('default task run!');
+			done(null);
+		});
+};
