094fb90de46023bd2b9cb35446301896f39fdab6
zhangaz1 <zhangaz1@hotmail.com>
2015-04-03 10:55:13 +0800

    配置build_server使用的路径

--- gulp/config.js
+++ gulp/config.js
@@ -1,6 +1,24 @@
 'use strict';
 
 
-module.exports = {
+var path = require('path');
+
+var root = path.resolve('./'); // process.cwd();
+
+var config = {
 	tasks: {}
 };
+
+config.files = {
+	server: joinRoot('./**/src/server/**/*.*')
+};
+
+config.directories = {
+	build: joinRoot('./build')
+};
+
+function joinRoot(subPath) {
+	return path.join(root, subPath);
+}
+
+module.exports = config;
--- gulp/tasks/build/server.js
+++ gulp/tasks/build/server.js
@@ -4,11 +4,19 @@
 module.exports = function(context, name) {
 	context.config.tasks[name] = name;
 
-	context.gulp.task(
+	var gulp = context.gulp;
+	var config = context.config;
+
+	gulp.task(
 		name,
 		'build server task',
 		function(done) {
-			console.log('task ' + name + ' run!');
+			console.log(config.files.server);
+			console.log(config.directories.build);
+
+			gulp.src(config.files.server)
+				.pipe(gulp.dest(config.directories.build));
+
 			done(null);
 		});
 };
