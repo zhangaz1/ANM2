85af2e8d04e9bc070448d428a79db8800a49791d
zhangaz1 <zhangaz1@hotmail.com>
2015-04-03 14:49:02 +0800

    临时测试

--- gulp/tasks/watch/server.js
+++ gulp/tasks/watch/server.js
@@ -11,19 +11,20 @@ module.exports = function(context, name) {
 		name,
 		'watch server to build when change',
 		function(done) {
-			addWatch();
+			console.log('watch server');
+			// addWatch();
 			done(null);
 		});
 
-	function addWatch() {
-		context.watchManager
-			.addWatchOnlyOnce(
-				name,
-				function() {
-					gulp.watch(config.files.server,
-						function() {
-							gulp.start(config.tasks.build_server);
-						});
-				});
-	}
+	// function addWatch() {
+	// 	context.watchManager
+	// 		.addWatchOnlyOnce(
+	// 			name,
+	// 			function() {
+	// 				gulp.watch(config.files.server,
+	// 					function() {
+	// 						gulp.start(config.tasks.build_server);
+	// 					});
+	// 			});
+	// }
 };
