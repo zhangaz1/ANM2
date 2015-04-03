8dc13e8edb00be648a992477e8aa9ce611e1a1cb
zhangaz1 <zhangaz1@hotmail.com>
2015-04-03 10:09:38 +0800

    使用gulp-help

--- gulp/context.js
+++ gulp/context.js
@@ -1,7 +1,14 @@
 'use strict';
 
 
+var gulp = require('gulp');
+var help = require('gulp-help');
+
+var config = require('./config.js');
+
+gulp = help(gulp);
+
 module.exports = {
-	gulp: require('gulp'),
-	config: require('./config.js')
+	gulp: gulp,
+	config: config
 };
