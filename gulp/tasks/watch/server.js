3891ea38c59aa9454ecd96403d168851bd18a856
zhangaz1 <zhangaz1@hotmail.com>
2015-04-03 14:27:39 +0800

    增加server的watch
    server代码变化时激发build_server任务

---
+++ gulp/tasks/watch/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+module.exports = function(context, name) {
+	context.config.tasks[name] = name;
+
+	var gulp = context.gulp;
+	var config = context.config;
+
+	gulp.task(
+		name,
+		'watch server to build when change',
+		function(done) {
+			addWatch();
+			done(null);
+		});
+
+	function addWatch() {
+		context.watchManager
+			.addWatchOnlyOnce(
+				name,
+				function() {
+					gulp.watch(config.files.server,
+						function() {
+							gulp.start(config.tasks.build_server);
+						});
+				});
+	}
+};
