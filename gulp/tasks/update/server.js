64b9c2cc14a3efe61619c487120551f049390cf9
zhangaz1 <zhangaz1@hotmail.com>
2015-04-06 14:36:38 +0800

    添加update_server任务

---
+++ gulp/tasks/update/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+module.exports = function(context, name) {
+	var tasks = context.config.tasks;
+
+	context.gulp
+		.task(
+			name,
+			'update server task',
+			function(done) {
+				context.sequence(
+					tasks.build_server,
+					tasks.runServer_web,
+					tasks.dev_reload,
+					done
+				);
+			}
+		);
+};
