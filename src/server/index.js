20570487a9fccb8e31e9e480a70a9cb4f20e151f
zhangaz1 <zhangaz1@hotmail.com>
2015-04-16 10:36:15 +0800

    server main by context

--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,31 @@
 
 var koa = require('koa');
 
-var context = require('./../../context.js');
 var staticResource = require('./handlers/common/staticResource.js');
 
-var app = koa();
-context.app = app;
 
-staticResource(context);
+module.exports = function(context) {
+	var app =
+		context.app = createApp(context);
 
-app.use(function*() {
-	this.body = 'hi!';
-})
+	addHandlers(context);
 
-app.listen(context.config.webPort);
+	runApp(context);
+};
+
+
+function createApp(context) {
+	return koa();
+}
+
+function addHandlers(context) {
+	staticResource(context);
+
+	context.app.use(function*() {
+		this.body = 'hi!';
+	})
+}
+
+function runApp(context) {
+	context.app.listen(context.config.webPort);
+}
