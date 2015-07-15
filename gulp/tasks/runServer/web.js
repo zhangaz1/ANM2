650eacf4db618a59ccd5598e22bc6cd32ff4c263
zhangaz1 <zhangaz1@hotmail.com>
2015-07-15 10:24:55 +0800

    启动web时增加-harmony参数

--- gulp/tasks/runServer/web.js
+++ gulp/tasks/runServer/web.js
@@ -4,41 +4,41 @@
 var exec = require('exec');
 
 module.exports = function(context, name) {
-	var tasks = context.config.tasks;
-	var cacheManager = context.cacheManager;
+    var tasks = context.config.tasks;
+    var cacheManager = context.cacheManager;
 
-	context.gulp
-		.task(
-			name,
-			'run web server task',
-			function(done) {
-				console.log('web server restarting...');
+    context.gulp
+        .task(
+            name,
+            'run web server task',
+            function(done) {
+                console.log('web server restarting...');
 
-				var webProcess = cacheManager.get(name);
-				if (webProcess) {
-					webProcess.kill();
-				}
+                var webProcess = cacheManager.get(name);
+                if (webProcess) {
+                    webProcess.kill();
+                }
 
-				webProcess = startWeb();
-				cacheManager.set(name, webProcess);
+                webProcess = startWeb();
+                cacheManager.set(name, webProcess);
 
-				console.log('web server restarted');
+                console.log('web server restarted');
 
-				process.nextTick(done);
-			}
-		);
+                process.nextTick(done);
+            }
+        );
 };
 
 function startWeb(argument) {
-	return exec(
-		['node', './build/index.js'],
-		function(err, out, code) {
-			if (err instanceof Error) {
-				// throw err;
-				console.log(err);
-			}
-			process.stderr.write(err);
-			process.stdout.write(out);
-		}
-	);
+    return exec(
+        ['node', '-harmony', './build/index.js'],
+        function(err, out, code) {
+            if (err instanceof Error) {
+                // throw err;
+                console.log(err);
+            }
+            process.stderr.write(err);
+            process.stdout.write(out);
+        }
+    );
 }
